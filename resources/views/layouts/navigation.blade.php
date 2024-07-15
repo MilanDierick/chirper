2f2b64283a3c28e7fb8562481bacf6f6e5110eb7
Milan Dierick <milan.dierick@proton.me>
2024-07-15 22:00:43 +0200

    Added local images for website logo.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,7 @@
         </div>
         <a href="{{ route('events.index') }}" class="-m-1.5 p-1.5">
             <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+            <img class="h-8 w-auto" src="{{asset('images/mark.svg')}}" alt="">
         </a>
         <div class="flex flex-1 justify-end">
             @auth
